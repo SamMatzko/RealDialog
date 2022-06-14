c87cf98212e345d1bd8f4149e246414a9ad43964
Samuel Matzko <matzko+sam@gmail.com>
2022-06-14 18:05:02 -0400

    🚧 Dialog can now select files and be canceled.

--- dialogs.py
+++ dialogs.py
@@ -13,6 +13,12 @@ class _FileDialog(tkinter.Toplevel):
     def __init__(self, master, *args, **kwargs):
         super().__init__(*args, master=master, **kwargs)
         
+        # The file/dir that's been selected. None if nothing was selected, or if Cancel was hit.
+        self.selected_file = None
+        
+        # The user's response (Ok, Cancel)
+        self.response = "placeholder"
+
         # The dialog's parent window
         self.master = master
 
@@ -37,18 +43,28 @@ class _FileDialog(tkinter.Toplevel):
         )
         self.dir_image = tkinter.PhotoImage(file=DIR_IMAGE).subsample(4)
 
-        # Create all the dialog's widgets, and set its theme
-        self.search_button = ttk.Button(self, text="Search...")
-        self.search_button.grid(row=0, column=0, sticky="W")
-        style = ttk.Style()
-        style.theme_use("clam")
+        # Create all the dialog's widgets
         self.__create_widgets()
         
         # Open the home directory
         self.__show_dir(os.environ["HOME"])
     
+    def __cancel(self, event=None):
+        """Cancel the dialog."""
+        self.response = False
+    
+    def __create_buttons(self):
+        """Create all the buttons for the file dialog."""
+
+        # The cancel button
+        self.cancel_button = ttk.Button(self, text="Cancel", underline=0, command=self.__cancel)
+        self.cancel_button.grid(row=0, column=0, sticky="w")
+    
     def __create_widgets(self):
         """Create all the widgets for the file dialog."""
+        
+        # All the buttons (warrant their own creation function)
+        self.__create_buttons()
         
         # The treeview for the files
         self.treeview = ttk.Treeview(self, columns=("size", "modified"))
@@ -75,6 +91,13 @@ class _FileDialog(tkinter.Toplevel):
         self.columnconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
     
+    def __on_dir_click(self, event=None):
+        self.__show_dir(self.treeview.selection()[0])
+    
+    def __on_file_click(self, event=None):
+        self.selected_file = self.treeview.selection()[0]
+        self.response = True
+        
     def __open_dir(self, directory):
         """Return two sorted lists (directories, files) of the contents of directory."""
         
@@ -114,7 +137,8 @@ class _FileDialog(tkinter.Toplevel):
                 d + os.path.sep,
                 text=os.path.basename(d),
                 image=self.dir_image,
-                values=(f"{len(os.listdir(d))} item(s)", f"{time.ctime(os.path.getmtime(d))}")
+                values=(f"{len(os.listdir(d))} item(s)", f"{time.ctime(os.path.getmtime(d))}"),
+                tags=("dir")
             )
 
         # Display the files
@@ -125,13 +149,31 @@ class _FileDialog(tkinter.Toplevel):
                 f,
                 text=os.path.basename(f),
                 image=self.file_image,
-                values=(f"{os.path.getsize(f)} bytes", f"{time.ctime(os.path.getmtime(f))}")
+                values=(f"{os.path.getsize(f)} bytes", f"{time.ctime(os.path.getmtime(f))}"),
+                tags=("file")
             )
+        
+        # Configure the event-handling for the files and directories
+        self.treeview.tag_bind("dir", "<Double-Button-1>", self.__on_dir_click)
+        self.treeview.tag_bind("file", "<Double-Button-1>", self.__on_file_click)
+    
+    def show(self):
+        """Show the dialog, and return a tuple for the (user_response, selected_file), user
+        response being True (proceed) or False (cancel)."""
+        while True:
+            try:
+                self.update()
+            except tkinter.TclError:
+                break
+            if self.response != "placeholder":
+                self.destroy()
+                break
+        return self.response, self.selected_file
 
 if __name__ == "__main__":
     # Testing
     root = tkinter.Tk()
-    button = tkinter.Button(root, text="_FileDialog", command=lambda: _FileDialog(root))
+    button = tkinter.Button(root, text="_FileDialog", command=lambda: print(_FileDialog(root).show()))
     button.pack()
     ttk.Style().theme_use("classic")
     root.mainloop()
